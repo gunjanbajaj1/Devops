6df03c4c7b3e0094e602f77969c1f797a8b9d58b
turbhekarprachi <turbhekarprachi@gmail.com>
2022-08-14 17:04:09 +0530

    nvm changes

--- nvm.sh
+++ nvm.sh
@@ -17,9 +17,7 @@ then
         echo "nvm version : $(nvm --version)"
         echo "node version : $(node --version)"
         echo "npm version : $(npm --version)"
-        export NVM_DIR="$HOME/.nvm"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+        export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
     fi
 else
     echo "NVM is already installed $(nvm --version)"
