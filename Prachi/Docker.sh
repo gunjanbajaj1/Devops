b99fc4cb88477564c21d4efff941aa7c1792f09f
turbhekarprachi <turbhekarprachi@gmail.com>
2022-08-22 00:14:03 +0530

    Docker script changes

--- Prachi/Docker.sh
+++ Prachi/Docker.sh
@@ -11,7 +11,7 @@ then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
     apt-cache policy docker-ce
-    sudo apt install docker-ce
+    sudo apt install -y docker-ce
     sudo systemctl status docker | grep running
     if [ $? -ne 0 ]
     then 
